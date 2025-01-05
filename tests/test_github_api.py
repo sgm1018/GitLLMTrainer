aafb1f09b2fcb8a708e1b5fac48d8a67eb5b8fd1
Sergio González Martínez <1018sgm1018@gmail.com>
2025-01-05 22:22:45 +0100

    fix: changed demo test urls

--- tests/test_github_api.py
+++ tests/test_github_api.py
@@ -7,11 +7,11 @@ class TestGitHubAPI(unittest.TestCase):
         self.api = GitHubAPI()
 
     def test_fetch_directory(self):
-        response = self.api.fetch_directory("https://api.github.com/repos/tldraw/tldraw/contents")
+        response = self.api.fetch_directory("https://api.github.com/repos/sgm1018/GitLLMTrainer/contents")
         self.assertIsInstance(response, list)
 
     def test_fetch_file_content(self):
-        content = self.api.fetch_file_content("https://raw.githubusercontent.com/tldraw/tldraw/main/.dockerignore")
+        content = self.api.fetch_file_content("https://raw.githubusercontent.com/sgm1018/GitLLMTrainer/main/README.md")
         self.assertIsInstance(content, str)
 
 

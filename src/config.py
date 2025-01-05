6e26c26381fd9b6e20e60255019d7ced7847beb1
Sergio González Martínez <1018sgm1018@gmail.com>
2025-01-05 21:56:05 +0100

    Fix: fix config

--- src/config.py
+++ src/config.py
@@ -1,3 +1,2 @@
 GITHUB_API_URL = "https://api.github.com"
-# GITHUB_TOKEN = None  # Reemplaza con tu token si deseas acceder a repos privados
-GITHUB_TOKEN =   # Reemplaza con tu token si deseas acceder a repos privados
+GITHUB_TOKEN = None  # Reemplaza con tu token si deseas acceder a repos privados
